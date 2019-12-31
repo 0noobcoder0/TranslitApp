6cbc8c3d258dd20a90da16395c2f321ef6adf9f0
Arman <armanstanite@yahoo.com>
2019-12-31 13:33:00 +0530

    production build

---
+++ build/precache-manifest.3580acb548f0e8ca9b03f9566076fec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2cfcea3ceeec1d4db2a69ee1f363af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa9d07a6eb08fc3c0db4",
+    "url": "/static/css/main.98d32858.chunk.css"
+  },
+  {
+    "revision": "9706b2a47cc3cb29029c",
+    "url": "/static/js/2.0c3a9553.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0c3a9553.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa9d07a6eb08fc3c0db4",
+    "url": "/static/js/main.c79fe004.chunk.js"
+  },
+  {
+    "revision": "919a2e1b19cef32203aa",
+    "url": "/static/js/runtime-main.867fc445.js"
+  }
+]);
